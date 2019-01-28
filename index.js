de3b0481b45c6596b3bfe79dd9be8001f060d481
Peter Nguyen <peternguyen160@gmail.com>
2019-01-28 05:11:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function dwarfRollCall(dwarves){
 	var array =[]
 	for (var i=0;i<dwarves.length;i++){
 		//array.push(`${i+1}. ${dwarves[i]} `)
-		array.push([i+1] + "." + dwarves[i])
+		array.push([i+1] + "." + " " dwarves[i])
 	}
 	return array.join("")
 }
